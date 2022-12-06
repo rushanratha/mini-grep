544a7f4affd8a4423f1132bd86f8273b4c9bace3
Rushan Ratha <28103376+echo-corp@users.noreply.github.com>
2022-12-06 20:13:21 +0000

    Add lib class for abstraction.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,18 @@
 use std::env;
+use std::process;
+
+use mini_grep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let query = &args[1];
-    let file_path = &args[2];
 
-    println!("Searching for {}", query);
-    println!("In file {}", file_path);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
+    if let Err(e) = mini_grep::run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
 }
